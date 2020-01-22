42a908c25cc5b30f09d1386748db5a5661b4f12c
Allen Shin <geeuho@gmail.com>
2020-01-22 09:25:11 -0800

    ignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,8 @@ class ApplicationController < ActionController::API
       def logged_in?
         !!current_user
       end
+
+      def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+      end
 end
