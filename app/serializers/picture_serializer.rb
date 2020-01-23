3ff460cfd97503b968de52b7adff4981fa304e65
Allen Shin <geeuho@gmail.com>
2020-01-22 18:26:42 -0800

    Wednesday

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -1,5 +1,5 @@
 class PictureSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :img_url, :roast_bio, :toast_bio, :user_id, :comments, :picture_likes, :picture_dislikes
+  attributes :img_url, :roast_bio, :toast_bio, :user_id, :user, :comments, :picture_likes, :picture_dislikes, :created_at
 end
  
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,15 @@
 # Dislike.destroy_all
 
 user1 = User.create(username: 'Dawnster', password: 'test123', password_confirmation: 'test123')
-user2 = User.create(username: 'Geeuho', password: 'test123', password_confirmation: 'test123')
+user2 = User.create(username: 'Allen', password: 'test123', password_confirmation: 'test123')
 user3 = User.create(username: 'Roaster', password: 'test123', password_confirmation: 'test123')
 user4 = User.create(username: 'Toaster', password: 'test123', password_confirmation: 'test123')
+user5 = User.create(username: 'geeuho', password: 'jeeho123', password_confirmation: 'jeeho123')
 
-picture1 = Picture.create(img_url:'www.sampleurl1.com', roast_bio: 'Roasting bio sample 1', toast_bio: 'Toasting bio sample 1', user_id: 1)
-picture2 = Picture.create(img_url:'www.sampleurl2.com', roast_bio: 'Roasting bio sample 2', toast_bio: 'Toasting bio sample 2', user_id: 2)
-picture3 = Picture.create(img_url:'www.sampleurl3.com', roast_bio: 'Roasting bio sample 3', toast_bio: 'Toasting bio sample 3', user_id: 3)
-picture4 = Picture.create(img_url:'www.sampleurl4.com', roast_bio: 'Roasting bio sample 4', toast_bio: 'Toasting bio sample 4', user_id: 4)
+picture1 = Picture.create(img_url:'https://s.hdnux.com/photos/01/10/07/51/18905699/3/920x920.jpg', roast_bio: 'Roasting bio sample 1', toast_bio: 'Toasting bio sample 1', user_id: 1)
+picture2 = Picture.create(img_url:'https://storage.googleapis.com/pokercentral/2019/12/dd51c1dc-3jbevg-815x500.jpg', roast_bio: 'Roasting bio sample 2', toast_bio: 'Toasting bio sample 2', user_id: 2)
+picture3 = Picture.create(img_url:'https://cdn.vox-cdn.com/thumbor/H3OFDKo_jSsQpICxKbhqzv1ellw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/9869735/teaser_pic.0.0.0.png', roast_bio: 'Roasting bio sample 3', toast_bio: 'Toasting bio sample 3', user_id: 3)
+picture4 = Picture.create(img_url:'https://d2slcw3kip6qmk.cloudfront.net/marketing/blogs/press/dont-ask/how_to_use_memes_for_marketing.jpg' , roast_bio: 'Roasting bio sample 4', toast_bio: 'Toasting bio sample 4', user_id: 4)
 
 comment1 = Comment.create(roast: true, text: 'Sample Text 1', picture_id: 1, user_id: 1)
 comment2 = Comment.create(roast: false, text: 'Sample Text 2', picture_id: 2, user_id: 1)
@@ -33,13 +34,22 @@ like2 = Like.create(user_id: 2, comment_id: 2)
 like3 = Like.create(user_id: 3, comment_id: 3)
 like4 = Like.create(user_id: 4, comment_id: 4)
 
+
 dislike1 = Dislike.create(user_id: 1, comment_id: 1)
 dislike2 = Dislike.create(user_id: 2, comment_id: 2)
 dislike3 = Dislike.create(user_id: 3, comment_id: 3)
 dislike4 = Dislike.create(user_id: 4, comment_id: 4)
 
-picturelike1 = PictureLike.create(user_id: 1, picture_id: 1)
+picturelike1 = PictureLike.create(user_id: 1, picture_id: 4)
 picturelike2 = PictureLike.create(user_id: 2, picture_id: 2)
+picturelike3 = PictureLike.create(user_id: 3, picture_id: 4)
+picturelike4 = PictureLike.create(user_id: 4, picture_id: 3)
+picturelike4 = PictureLike.create(user_id: 5, picture_id: 4)
+picturelike4 = PictureLike.create(user_id: 5, picture_id: 3)
 
-picturedislike1 = PictureDislike.create(user_id: 1, picture_id: 1)
-picturedislike2 = PictureDislike.create(user_id: 2, picture_id: 2)
+picturedislike1 = PictureDislike.create(user_id: 1, picture_id: 3)
+picturedislike2 = PictureDislike.create(user_id: 2, picture_id: 4)
+picturedislike3 = PictureDislike.create(user_id: 3, picture_id: 3)
+picturedislike4 = PictureDislike.create(user_id: 4, picture_id: 1)
+picturedislike4 = PictureDislike.create(user_id: 5, picture_id: 3)
+picturedislike4 = PictureDislike.create(user_id: 5, picture_id: 1)
