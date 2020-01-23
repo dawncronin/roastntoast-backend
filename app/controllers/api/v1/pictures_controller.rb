3fa247bbdcb3803933354db7441ca3c05ab0fc6f
dawncronin <github email address>
2020-01-22 21:14:44 -0800

    fixed some post bugs

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::PicturesController < ApplicationController
         picture = Picture.find(params[:id])
         render json: PictureSerializer.new(picture)
     end
+
+    def create
+        picture = Picture.create(picture_params)
+        if picture.valid?
+            render json: { picture: PictureSerializer.new(picture)}, status: :created
+        else
+            render json: { error: 'failed to create picture' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def picture_params
+        params.require(:picture).permit(:img_url, :roast_bio, :toast_bio, :user_id)
+    end
 end
--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -1,6 +1,6 @@
 class PictureSerializer
   include FastJsonapi::ObjectSerializer
-  has_many :gitcomments
+  has_many :comments
   attributes :img_url, :roast_bio, :toast_bio, :user, :user_id, :comments, :picture_likes, :picture_dislikes
 end
  
